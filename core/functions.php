7431c9d0d911356c4ade2d28ea449873883be6f5
Alexander James Becoy <bec_alexanderjames@outlook.com>
2018-01-16 14:45:44 +0100

    v0.5

--- core/functions.php
+++ core/functions.php
@@ -8,32 +8,57 @@ function retrieveGenresInSelect($database) {
     $query = $database->query("SELECT * FROM Genre WHERE genre_name != 'Sci-Fi'");
 
     while($genre = $query->fetch()) {
-        echo '<option value="genre_'.strtolower($genre['genre_name']).'">'.$genre['genre_name'].'</option>';
+        echo '<option value="genre_'.$genre['genre_name'].'">'.$genre['genre_name'].'</option>';
     }
 }
 
-function pagination($destination, $page=1) {
+function pagination($destination, $page, $pageLimit) {
     $pagination = '<div class="pagination">';
-    $pagination .= '<a href="'.$destination.'?page='.($page - 1).'">&laquo;</a>';
-    for($pageIndex = $page; $pageIndex < $page + 5; $pageIndex++) {
-        $pagination .= '<a href="'.$destination.'?page='.$pageIndex.'">'.$pageIndex.'</a>';
+    if($page <= 3) {
+
+        for($pageIndex = 1; $pageIndex <= 5; $pageIndex++) {
+            $active = ($pageIndex == $page)? 'active' : '';
+            $pagination .= '<a class="'.$active.'" href="'.$destination.'?page='.$pageIndex.'">'.$pageIndex.'</a>';
+        }
+        $pagination .= '<a href="'.$destination.'?page='.($page + 1).'">&raquo;</a>';
+        $pagination .= '<a href="'.$destination.'?page='.$pageLimit.'">&raquo; Last</a></div>';
+
+    } elseif($page >= $pageLimit - 2) {
+
+        $pagination .= '<a href="'.$destination.'?page=1">First &laquo;</a>';
+        $pagination .= '<a href="'.$destination.'?page='.($page - 1).'">&laquo;</a>';
+        for($pageIndex = $pageLimit - 4; $pageIndex <= $pageLimit; $pageIndex++) {
+            $active = ($pageIndex == $page)? 'active' : '';
+            $pagination .= '<a class="'.$active.'" href="'.$destination.'?page='.$pageIndex.'">'.$pageIndex.'</a>';
+        }
+
+    } else {
+
+        $pagination .= '<a href="'.$destination.'?page=1">First &laquo;</a>';
+        $pagination .= '<a href="'.$destination.'?page='.($page - 1).'">&laquo;</a>';
+        for($pageIndex = $page - 2; $pageIndex <= $page + 2; $pageIndex++) {
+            $active = ($pageIndex == $page)? 'active' : '';
+            $pagination .= '<a class="'.$active.'" href="'.$destination.'?page='.$pageIndex.'">'.$pageIndex.'</a>';
+        }
+        $pagination .= '<a href="'.$destination.'?page='.($page + 1).'">&raquo;</a>';
+        $pagination .= '<a href="'.$destination.'?page='.$pageLimit.'">&raquo; Last</a></div>';
+
     }
-    $pagination .= '<a href="'.$destination.'?page='.($page + 1).'">&raquo;</a></div>';
     return $pagination;
 }
 
-function retrieveMoviesInBox($database, $page=1) {
+function retrieveMoviesInBox($database, $page=1, $genre='') {
     $rowCount = 0;
     $rowLimit = $page * 15;
-    $query = $database->query('SELECT * FROM Movie WHERE movie_id IN (SELECT movie_id FROM Movie_Genre WHERE genre_name = \'Sci-Fi\')');
+    $query = $database->query('SELECT * FROM Movie WHERE movie_id IN (SELECT movie_id FROM Movie_Genre WHERE genre_name = \'Sci-Fi\') AND movie_id IN (SELECT movie_id FROM Movie_Genre WHERE genre_name='.$genre.')');
     $pageLimit = (int)(count($query->fetchAll()) / 15);
-    $query = $database->query('SELECT * FROM Movie WHERE movie_id IN (SELECT movie_id FROM Movie_Genre WHERE genre_name = \'Sci-Fi\')');
+    $query = $database->query('SELECT * FROM Movie WHERE movie_id IN (SELECT movie_id FROM Movie_Genre WHERE genre_name = \'Sci-Fi\') AND movie_id IN (SELECT movie_id FROM Movie_Genre WHERE genre_name='.$genre.')');
     if(count($query->fetchAll()) % 15 > 0) $pageLimit += 1;
     if($page > $pageLimit || $page < 1) {
         header("Location: error");
     }
 
-    $query = $database->query('SELECT * FROM Movie WHERE movie_id IN (SELECT movie_id FROM Movie_Genre WHERE genre_name = \'Sci-Fi\') ORDER BY title ASC');
+    $query = $database->query('SELECT * FROM Movie WHERE movie_id IN (SELECT movie_id FROM Movie_Genre WHERE genre_name = \'Sci-Fi\') AND movie_id IN (SELECT movie_id FROM Movie_Genre WHERE genre_name='.$genre.') ORDER BY title ASC');
     while($movie = $query->fetch()) {
         if(($rowCount > ($page - 1) * 15) && ($rowCount <= $rowLimit)) {
             if($movie['cover_image'] == NULL) {
@@ -51,7 +76,7 @@ function retrieveMoviesInBox($database, $page=1) {
         $rowCount++;
     }
 
-    echo pagination('films', $page);
+    echo pagination('films', $page, $pageLimit);
 }
 
 ?>
